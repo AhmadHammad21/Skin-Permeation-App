cda69b0bc088ace1dc57fe424ab4b939fed8fb3d
AhmadHammad21 <ahmedhammad8765@gmail.com>
2024-12-10 21:03:50 +0300

    Modified Command Process

--- convert_java_to_python.py
+++ convert_java_to_python.py
@@ -4,11 +4,15 @@ import os
 
 
 def extract_descriptors(smiles: str):
-    command = "java -jar DescriptorsGeneratorCLI-1.0.0.jar {}".format(smiles)
+    # command = "java -jar DescriptorsGeneratorCLI-1.0.0.jar {}".format(smiles)
+    command = ["java", "-jar", "DescriptorsGeneratorCLI-1.0.0.jar", smiles]
+
     print("command")
     print(command)
-    generated_descriptors = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
-    
+    # generated_descriptors = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    generated_descriptors = result.stdout
+
     descriptors_list = str(generated_descriptors).split(",")
     
     cleaned_smiles = str(descriptors_list[0]).replace("\r", "").replace("\\n", "").replace("b\'", "")
